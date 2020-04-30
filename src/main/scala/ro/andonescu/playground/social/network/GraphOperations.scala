934acca132cc2fa955dcbd83cb326ca1bb9e5736
Ionut Andonescu <andonescu@gmail.com>
2020-04-30 08:49:05 +0300

    renaming of successors to `friendsSeq`

--- src/main/scala/ro/andonescu/playground/social/network/GraphOperations.scala
+++ src/main/scala/ro/andonescu/playground/social/network/GraphOperations.scala
@@ -20,14 +20,14 @@ object GraphOperations {
   def chainOfFriendsBetween[N](start: N, end: N)(context: Graph[N, GraphEdge.UnDiEdge]): Seq[N] = {
 
     val startNode = context.find(start).get
-    val successors = startNode.diSuccessors.toSeq
+    val friendsSeq = startNode.diSuccessors.toSeq
 
-    val friendsMap = collection.mutable.Map(successors.map(_ -> Seq(startNode)): _*)
+    val friendsMap = collection.mutable.Map(friendsSeq.map(_ -> Seq(startNode)): _*)
 
-    //1. go step by step, through all the `friends` of each node; a breadth-first search algorithm
+    //1. go step by step, through all the `friendsSeq` of each node; a breadth-first search algorithm
     @tailrec
-    def iterateThroughFriends(successors: Seq[context.NodeT]): Seq[context.NodeT] =
-      successors match {
+    def iterateThroughFriends(friendsSeq: Seq[context.NodeT]): Seq[context.NodeT] =
+      friendsSeq match {
         case head :: _ if head.value == end => friendsMap.get(head).getOrElse(Seq.empty) :+ head
         case head :: tail => {
           // get head successors which are not already present in our seq
@@ -35,7 +35,7 @@ object GraphOperations {
           // for each new head successor we will add in the map the proper steps to get to it
           val pathThroughFriendsToHead = friendsMap.get(head).getOrElse(Seq.empty) :+ head
           // global based knowledge, add the details about the new links
-          friendsMap ++=  friendsNotVisited.map(_ -> pathThroughFriendsToHead)
+          friendsMap ++= friendsNotVisited.map(_ -> pathThroughFriendsToHead)
 
           iterateThroughFriends(tail ++ friendsNotVisited)
         }
@@ -44,7 +44,7 @@ object GraphOperations {
 
     //2. TODO: further optimisation, start both ways (from the start point & end point) and see what matches appear
 
-    iterateThroughFriends(successors).map(_.value)
+    iterateThroughFriends(friendsSeq).map(_.value)
   }
 
 }
